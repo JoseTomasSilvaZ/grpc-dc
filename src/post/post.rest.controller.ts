997b3d5d723e3f9d98b0e805bec399a2f00fcfbe
josetomassilvaz <josetomassilvaz@gmail.com>
2024-04-20 17:39:56 -0400

    feat: add source

--- src/post/post.rest.controller.ts
+++ src/post/post.rest.controller.ts
@@ -3,7 +3,7 @@ import { Controller, Get, Param } from '@nestjs/common';
 import { Client, ClientGrpc, Transport } from '@nestjs/microservices';
 import { Observable } from 'rxjs';
 import { Post, Research } from '@prisma/client';
-import { ClassicCacheService } from './cache/classic.cache.service';
+import { RetrievedPost } from './types';
 import { ReplicatedCacheService } from './cache/replicated.cache.service';
 
 @Controller('posts')
@@ -21,7 +21,7 @@ export class PostRestController {
   private postService: any;
 
   constructor(
-    private replicatedCacheService: ReplicatedCacheService,
+    private replicatedCacheService: ReplicatedCacheService
   ) {}
 
   onModuleInit() {
@@ -29,20 +29,21 @@ export class PostRestController {
   }
 
   @Get('replicated-cache/:id')
-  async getPost(@Param('id') id: number): Promise<Research & {fromCache:boolean}> {
-    console.log(`Fetching post with ID: ${id}`);
+  async getPost(@Param('id') id: number): Promise<RetrievedPost> {
     let post = await this.replicatedCacheService.getPost(id);
+    console.log('Post from Redis:', post);
     let fromCache = true;
     if (!post) {
       try {
-        const postObservable: Observable<Research> = this.postService.findOne({id});
-        post = await new Promise<Research>((resolve, reject) =>
+        const postObservable: Observable<RetrievedPost> = this.postService.findOne({id});
+        post = await new Promise<RetrievedPost>((resolve, reject) =>
           postObservable.subscribe({
             next: resolve,
             error: reject
           })
         );
-        await this.replicatedCacheService.setPost(post);
+        console.log('Post from gRPC service:', post);
+        await this.replicatedCacheService.setPost(post.post);
         fromCache = false;
       } catch (error) {
         console.error('Error fetching post from gRPC service:', error);
